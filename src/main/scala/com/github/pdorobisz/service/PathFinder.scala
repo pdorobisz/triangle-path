c722a15ab7ab16a9dacc596da340753fb5ca1d7d
Piotr Dorobisz <pdorobisz@gmail.com>
2020-05-15 12:13:18 +0200

    Implement path finding

--- src/main/scala/com/github/pdorobisz/service/PathFinder.scala
+++ src/main/scala/com/github/pdorobisz/service/PathFinder.scala
@@ -3,13 +3,37 @@ package com.github.pdorobisz.service
 import com.github.pdorobisz.model.{RegularNode, RootNode, Triangle}
 
 trait PathFinder {
-  def minPath(t: Triangle): Seq[Int]
+  def minPath(leafLayer: List[Triangle]): List[Int]
 }
 
 object DefaultPathFinder extends PathFinder {
-  override def minPath(t: Triangle): Seq[Int] = t match {
-    case RootNode(value) => Seq(value)
-    case RegularNode(value, leftParent, rightParent) => Seq(value)
+  override def minPath(leafLayer: List[Triangle]): List[Int] = {
+    @scala.annotation.tailrec
+    def findMinPath(currentLayer: List[Triangle], bestSubtriangles: List[(Int, List[Int])]): List[Int] =
+      currentLayer match {
+        case (r: RootNode) :: Nil =>
+          if (bestSubtriangles.isEmpty) List(r.value)
+          else {
+            val bestChild = bestSubtriangles.minBy(_._1)
+            r.value :: bestChild._2
+          }
+        case childrenLayer: List[RegularNode] =>
+          val bestPath: List[(Int, List[Int])] = bestSubtriangles.sliding(2).zip(childrenLayer).map { case (children, n) =>
+            val bestChild = children.minBy(_._1)
+            (n.value + bestChild._1, n.value :: bestChild._2)
+          }.toList
+          val parentLayer = childrenLayer.flatMap(_.rightParent)
+          findMinPath(parentLayer, bestPath)
+      }
+
+    leafLayer match {
+      case (r: RootNode) :: Nil => List(r.value)
+      case list: List[RegularNode] =>
+        val values = list.map(n => (n.value, List(n.value)))
+        val parentLayer = list.flatMap(_.rightParent)
+
+        findMinPath(parentLayer, values)
+    }
   }
 
 }
--- src/test/scala/com/github/pdorobisz/service/DefaultPathFinderTest.scala
+++ src/test/scala/com/github/pdorobisz/service/DefaultPathFinderTest.scala
@@ -1,16 +1,38 @@
 package com.github.pdorobisz.service
 
-import com.github.pdorobisz.model.RootNode
+import com.github.pdorobisz.model.{RegularNode, RootNode}
 import org.scalatest.{FunSpec, Matchers}
 
 class DefaultPathFinderTest extends FunSpec with Matchers {
 
   describe("Default path finder") {
     describe("when finding min path") {
-      it("should return correct result for a triangle with one node only") {
+      it("should return correct result for a triangle with single node") {
         val input = RootNode(123)
-        val result = DefaultPathFinder.minPath(input)
+        val result = DefaultPathFinder.minPath(List(input))
         result shouldBe Seq(input.value)
+      }
+
+      it("should return correct result for a triangle with 2 level") {
+        val root = RootNode(1)
+        val leaf1 = RegularNode(3, None, Some(root))
+        val leaf2 = RegularNode(2, Some(root), None)
+
+        val result = DefaultPathFinder.minPath(List(leaf1, leaf2))
+        result shouldBe Seq(root.value, leaf2.value)
+      }
+
+      it("should return correct result for a triangle with 3 levels") {
+        val root = RootNode(7)
+        val node1 = RegularNode(6, None, Some(root))
+        val node2 = RegularNode(3, Some(root), None)
+
+        val leaf1 = RegularNode(3, None, Some(node1))
+        val leaf2 = RegularNode(8, Some(node1), Some(node2))
+        val leaf3 = RegularNode(5, Some(node2), None)
+
+        val result = DefaultPathFinder.minPath(List(leaf1, leaf2, leaf3))
+        result shouldBe Seq(root.value, node2.value, leaf3.value)
       }
     }
   }
